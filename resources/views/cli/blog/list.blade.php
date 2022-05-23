b69c7d4ded94baf253f94c42da0eb67b3e454bb7
GioPan04 <gioele@pannetto.com>
2022-05-23 23:32:10 +0200

    Updated page

--- resources/views/cli/blog/list.blade.php
+++ resources/views/cli/blog/list.blade.php
@@ -2,6 +2,6 @@
 
 @include('cli.common.prompt', ['command' => 'ls', 'path' => '~/blog'])
 @foreach ($posts as $post)
--rw-r--r-- gioele gioele {{ Carbon\Carbon::parse($post->created_at)->format('M d Y H:m')}} {!! AnsiCodes::BOLD_GREEN !!}{{ $post->slug }}{!! AnsiCodes::RESET !!}
+-rw-rw-r-- gioele users {{ Carbon\Carbon::parse($post->created_at)->format('M d Y H:m')}} {!! AnsiCodes::BOLD_GREEN !!}{{ $post->slug }}{!! AnsiCodes::RESET !!}
 @endforeach
 
