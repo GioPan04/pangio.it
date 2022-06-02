c6b2a2150194b0bd21c6e5d9de5c78922ceb8688
GioPan04 <gioele@pannetto.com>
2022-06-03 00:19:37 +0200

    Fix theme

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -4,10 +4,10 @@
 @section('page::body')
 <div class="container mx-auto">
     <div class="text-white/60 text-sm flex justify-between my-4">
-        <a href="{{ route('blog::show', ['slug' => $post->slug]) }}">{{ $post->slug }}</a>
-        <span title="{{ $post->created_at->format('d/m/Y H:M') }}">Published at {{ $post->created_at->format('d/m/Y') }}</span>
+        <a class="hover:underline" href="{{ route('blog::show', ['slug' => $post->slug]) }}">{{ $post->slug }}</a>
+        <span title="{{ $post->created_at->format('d/m/Y H:i') }}">Published at {{ $post->created_at->format('d/m/Y') }}</span>
     </div>
-    <div class="prose max-w-none prose-pre:p-4 prose-headings:text-white prose-a:text-white">
+    <div class="prose max-w-none prose-pre:p-4 prose-invert">
         {!! app(Spatie\LaravelMarkdown\MarkdownRenderer::class)->toHtml($post->content()) !!}
     </div>
 </div>
