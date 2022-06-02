d74d7d6bc55f0f3c07ac2df2f651ffcbd1597dc9
GioPan04 <gioele@pannetto.com>
2022-06-02 19:55:24 +0200

    Added date in post

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -3,7 +3,11 @@
 
 @section('page::body')
 <div class="container mx-auto">
-    <div class="prose max-w-none prose-pre:p-4 prose-headings:text-white">
+    <div class="text-white/60 text-sm flex justify-between my-4">
+        <a href="{{ route('blog::show', ['slug' => $post->slug]) }}">{{ $post->slug }}</a>
+        <span title="{{ $post->created_at->format('d/m/Y H:M') }}">Published at {{ $post->created_at->format('d/m/Y') }}</span>
+    </div>
+    <div class="prose max-w-none prose-pre:p-4 prose-headings:text-white prose-a:text-white">
         {!! app(Spatie\LaravelMarkdown\MarkdownRenderer::class)->toHtml($post->content()) !!}
     </div>
 </div>
