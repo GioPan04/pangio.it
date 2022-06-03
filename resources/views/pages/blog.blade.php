77b0f2207f82bb262cebd5683455778e019f075e
GioPan04 <gioele@pannetto.com>
2022-06-03 08:30:00 +0200

    Blog page

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -2,7 +2,13 @@
 @section('page::title', 'Blog')
 
 @section('page::body')
-@foreach ($posts as $post)
-    {{ $post->title }}
-@endforeach
+<main class="container mx-auto">
+    @foreach ($posts as $post)
+    <a href="{{ $post->link }}">
+        <h3 class="text-2xl font-bold">{{ $post->title }}</h3>
+        <span class="text-sm block">Published at {{ $post->created_at->format('d/m/Y') }}</span>
+        <span>{{ $post->description }}</span>
+    </a>
+    @endforeach
+</main>
 @endsection
--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -4,7 +4,7 @@
 @section('page::body')
 <div class="container mx-auto">
     <div class="text-white/60 text-sm flex justify-between my-4">
-        <a class="hover:underline" href="{{ route('blog::show', ['slug' => $post->slug]) }}">{{ $post->slug }}</a>
+        <a class="hover:underline" href="{{ $post->link }}">{{ $post->slug }}</a>
         <span title="{{ $post->created_at->format('d/m/Y H:i') }}">Published at {{ $post->created_at->format('d/m/Y') }}</span>
     </div>
     <div class="prose max-w-none prose-pre:p-4 prose-invert">
