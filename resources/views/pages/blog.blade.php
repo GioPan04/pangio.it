cf1fe6bd9ff39a681e222b2d45b2c43d5000a818
GioPan04 <gioele@pannetto.com>
2022-06-07 13:14:07 +0200

    No posts

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -4,12 +4,16 @@
 @section('page::body')
 @include('common.socialbar')
 <main class="container mx-auto">
-    @foreach ($posts as $post)
+    @forelse ($posts as $post)
     <a href="{{ $post->link }}">
         <h3 class="text-2xl font-bold">{{ $post->title }}</h3>
         <span class="text-sm block">Published at {{ $post->created_at->format('d/m/Y') }}</span>
         <span>{{ $post->description }}</span>
     </a>
-    @endforeach
+    @empty
+    <div class="h-screen grid justify-center items-center">
+        <span class="text-white/60">No posts available</span>
+    </div>
+    @endforelse
 </main>
 @endsection
