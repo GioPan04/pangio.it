2044fbaab26ff20df99803a5609646ec1080e407
GioPan04 <gioele@pannetto.com>
2022-06-07 17:02:51 +0200

    Home mobile friendly

--- resources/views/common/timeline.blade.php
+++ resources/views/common/timeline.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-timeline timeline">
+<div class="hidden md:grid grid-cols-timeline timeline">
     @foreach ($items as $item)
         @if($loop->index % 2 == 0)
         <div class="timeline-content pt-4 pb-16 last:pb-0">{{ $item['data'] }}</div>
@@ -17,5 +17,14 @@
         <div></div>
         @endif
     @endforeach
+</div>
 
+<div class="flex flex-col md:hidden">
+    @foreach ($items as $item)
+    <div class="mt-4">
+        <span class="mx-auto mb-2 block rounded-full h-16 w-16 bg-white text-black px-2 py-[18px] text-center font-bold text-xl">{{ $item['year'] }}</span>
+
+        <div>{{ $item['data'] }}</div>
+    </div>
+    @endforeach
 </div>
