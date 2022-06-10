e2fa9064880073e124cf7cf5db160405fdadd947
GioPan04 <gioele@pannetto.com>
2022-06-10 12:21:44 +0200

    Added aria label to social bar

--- resources/views/common/socialbar.blade.php
+++ resources/views/common/socialbar.blade.php
@@ -1,26 +1,26 @@
 <ul class="hidden lg:block fixed bottom-3 left-3 space-y-3 z-10">
     <li class="opacity-50 hover:opacity-100 transition-opacity duration-200">
-        <a href="https://github.com/GioPan04" target="_blank">
+        <a href="https://github.com/GioPan04" target="_blank" aria-label="GitHub">
             <i data-feather="github"></i>
         </a>
     </li>
     <li class="opacity-50 hover:opacity-100 transition-opacity duration-200">
-        <a href="mailto:gioele@pannetto.com" target="_blank">
+        <a href="mailto:gioele@pannetto.com" target="_blank" aria-label="Email">
             <i data-feather="mail"></i>
         </a>
     </li>
     <li class="opacity-50 hover:opacity-100 transition-opacity duration-200">
-        <a href="https://codepen.io/GioPan" target="_blank">
+        <a href="https://codepen.io/GioPan" target="_blank" aria-label="Codepen">
             <i data-feather="codepen"></i>
         </a>
     </li>
     <li class="opacity-50 hover:opacity-100 transition-opacity duration-200">
-        <a href="https://twitter.com/giopan_sh" target="_blank">
+        <a href="https://twitter.com/giopan_sh" target="_blank" aria-label="Twitter">
             <i data-feather="twitter"></i>
         </a>
     </li>
     <li class="opacity-50 hover:opacity-100 transition-opacity duration-200">
-        <a href="https://instagram.com/giopan.sh" target="_blank">
+        <a href="https://instagram.com/giopan.sh" target="_blank" aria-label="Instagram">
             <i data-feather="instagram"></i>
         </a>
     </li>
