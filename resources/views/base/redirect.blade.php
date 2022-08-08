58f11a0ee9fbcc3a3dc1f761a605136533d37f96
GioPan04 <gioele@pannetto.com>
2022-08-08 17:59:14 +0200

    Fix link tracking

--- resources/views/base/redirect.blade.php
+++ resources/views/base/redirect.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <meta http-equiv="refresh" content="0; url={{ $url }}"/>
+    <meta http-equiv="refresh" content="1; url={{ $url }}"/>
     <title>Redirecting</title>
 </head>
 <body>
