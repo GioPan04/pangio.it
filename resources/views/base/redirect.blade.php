44ffc3af354521f93d5c1d08d64c5b622e2eecd8
GioPan04 <gioele@pannetto.com>
2022-08-08 18:07:57 +0200

    Customized redirect page

--- resources/views/base/redirect.blade.php
+++ resources/views/base/redirect.blade.php
@@ -6,9 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <meta http-equiv="refresh" content="1; url={{ $url }}"/>
     <title>Redirecting</title>
+    <style>
+        body {
+            background-color: #0D1821;
+            color: #fff;
+            font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        }
+        span {
+            text-align: center;
+            display: block;
+        }
+    </style>
 </head>
 <body>
-
+<span>Redirecting to {{$url}}</span>
 <script src="{{ mix('js/redirect.js') }}" defer=""></script>
 </body>
 </html>
