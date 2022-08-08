ed0b9953057cb570924c3153d4074ca4f594f742
GioPan04 <gioele@pannetto.com>
2022-08-08 17:13:21 +0200

    Removed Google Fonts from error pages

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -4,9 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('/css/web.css')}}"/>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&family=Press+Start+2P&display=swap" rel="stylesheet">
         <title>@yield('title') - Gioele Pannetto</title>
     </head>
     <body class="bg-primary font-body">
